11e18eda9d528da90d5e470ab1ceb30447e1b33b
Nickerm <nick.ermolenkov@gmail.com>
2021-07-29 13:56:45 +0300

    feat: add first hook

--- itk/react-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ itk/react-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Preloader from '../../Common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
-import ProfileStatus from './ProfileStatus'
+import ProfileStatusWithHooks from './ProfileStatusWithHooks'
 
 const ProfileInfo = (props) => {
 
@@ -27,7 +27,7 @@ const ProfileInfo = (props) => {
         <div> Full name : {props.profile.fullName}</div>
         <div> Contacts :</div>
        
-        <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
       </div>
     </div>
   );
--- itk/react-1/src/redux/usersSelectors.js
+++ itk/react-1/src/redux/usersSelectors.js
@@ -4,13 +4,13 @@ export const getUsers = (state) => {
     return state.usersPage.users;
 }
 
-export const getUsersSelector = (state) => {
-    return getUsers(state).filter(u => true);
-}
+// export const getUsersSelector = (state) => {
+//     return getUsers(state).filter(u => true);
+// }
 //reselect call render when users is changed
-export const getUsersSuperSelector = createSelector(getUsers, getFetchingStatus, (users, isFetching) => {
-    return users.filter(u => true);
-}) 
+// export const getUsersSuperSelector = createSelector(getUsers, getFetchingStatus, (users, isFetching) => {
+//     return users.filter(u => true);
+// }) 
 
 export const getPageSize = (state) => {
     return state.usersPage.pageSize;
